e1688efcaa3b5c5e11500fde70d05dedc5f9d6a2
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-02-20 20:14:56 -0300

    :construction: Deleta os todos

--- src/Todos.js
+++ src/Todos.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 
-const Todos = ({todos}) => {
+const Todos = ({todos, deleteTodo}) => {
 
+    // Se todoList estiver vazio, irá retornar a mensagem 'You have no todo's left, yay!'
     const todoList = todos.length ? (
         todos.map(todo => {
             return (
                 <div className="collection-item" key={todo.id}>
-                    <span>{todo.content}</span>
+                    <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
                 </div>
             )
         })
